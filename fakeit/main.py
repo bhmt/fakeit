ec3c0cc62b69f29bf798e5e22a55940d6cc50b6d
roko@bebop <roko.mislov.m@gmail.com>
2021-03-13 15:17:23 +0100

    api: FastAPI first step added. Now using poetry
    The FastAPI instance is created and served using uvicorn.
    For now, it uses an empty router where the genrated routes will be dynamically
    added.
    Poetry is used as a package manager.

--- fakeit/main.py
+++ fakeit/main.py
@@ -1,17 +1,21 @@
 import sys
+import uvicorn
+from fastapi import FastAPI
 from reader import get_data
+from api.router import api_router
 
+filepath = sys.argv[-1]
+(data, success) = get_data(filepath)
 
-if __name__ == '__main__':
-    filepath = sys.argv[-1]
-    (data, success) = get_data(filepath)
+if success is False:
+    print(data)
+    sys.exit(-1)
 
-    if success is False:
-        print(data)
-        sys.exit(-1)
+title = data.title or "FakeIt"
+description = data.description or "Return pseudorandom data"
+paths = data.paths
 
-    title = data.title or "FakeIt"
-    description = data.description or "Return pseudorandom data"
+app = FastAPI(title=title, description=description)
+app.include_router(api_router)
 
-    print(title)
-    print(description)
+uvicorn.run(app, host="0.0.0.0", port=9876, log_level="info")
