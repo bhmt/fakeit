d66614fb35fc4d79fff30e14950460524ea764c9
roko@bebop <roko.mislov.m@gmail.com>
2021-03-14 10:29:57 +0100

    validation for interval added

--- fakeit/models/definition.py
+++ fakeit/models/definition.py
@@ -8,22 +8,57 @@ base_types = ("boolean", "number", "string", "object")
 def __validate_array_type__(v: str) -> (str, bool):
     deconstruct = v.split(":")
     msg = []
-    if deconstruct[1] not in base_types:
-        msg.append(f"argument type value '{v}' is not allowed in array.")
+
+    if len(deconstruct) == 1:
+        msg.append(f"array must have a defined inner type")
+    elif deconstruct[1] not in base_types:
+        msg.append(f"argument type value '{v}' is not allowed in array")
+
     if len(deconstruct) > 2:
         try:
             int(deconstruct[2])
         except ValueError:
-            msg.append(f"inner interval value '{deconstruct[2]}' not allowed.")
+            msg.append(f"inner interval value '{deconstruct[2]}' not allowed")
         try:
             int(deconstruct[3])
         except IndexError:
             pass
         except ValueError:
-            msg.append(f"inner interval value '{deconstruct[3]}' not allowed.")
+            msg.append(f"inner interval value '{deconstruct[3]}' not allowed")
 
     if msg:
         return ('\n'.join(msg), False)
+    return ("", True)
+
+
+def __validate_interval_as_string__(v: str) -> (str, bool):
+    msg = []
+    deconstruct = v.split(":")
+
+    if deconstruct[0] == "":
+        try:
+            int(deconstruct[1])
+        except IndexError:
+            msg.append("must have upper limit")
+        except ValueError:
+            msg.append(f"interval value '{deconstruct[1]}' not allowed")
+    else:
+        try:
+            tmp = int(deconstruct[0])
+        except ValueError:
+            msg.append(f"interval value '{deconstruct[0]}' not allowed")
+        try:
+            int(deconstruct[1])
+        except IndexError:
+            pass
+        except ValueError:
+            if deconstruct[1]:
+                msg.append(f"interval value '{deconstruct[1]}' not allowed")
+            else:
+                msg.append("must have upper limit")
+
+    if msg:
+        return ("\n".join(msg), False)
     return ("", True)
 
 
@@ -35,13 +70,22 @@ class Definition(BaseModel):
 
     @validator('type')
     def allowed_types(cls, v: str):
-        if v.lower().startswith("array:"):
+        if v.lower().startswith("array"):
             msg, success = __validate_array_type__(v)
             if success is False:
                 raise ValueError(msg)
+        elif v.lower() not in base_types:
+            raise ValueError(f"argument type value '{v}' is not allowed")
+
+        return v
+
+    @validator('interval')
+    def allowed_interval(cls, v: Union[str, int]):
+        if v is None or isinstance(v, int):
             return v
-        if v.lower() not in base_types:
-            raise ValueError(f"argument type value '{v}' is not allowed.")
+        msg, success = __validate_interval_as_string__(v)
+        if success is False:
+            raise ValueError(msg)
 
         return v
 
