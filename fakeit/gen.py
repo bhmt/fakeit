30f1d8553fe5f3cd6ae9893ab6c5ea152e316689
roko@bebop <roko.mislov.m@gmail.com>
2021-03-14 16:41:38 +0100

    generating complex objects
    The obj function in gen.py now returns a complex object defained by the
    type `object`.

--- fakeit/gen.py
+++ fakeit/gen.py
@@ -40,19 +40,46 @@ def array(definition: Definition) -> Union[Dummy, bool, str, float, list]:
     return base(definition.name, value)
 
 
-def obj(definition: Definition):
-    ...
+def obj(definition: Definition) -> Dummy:
+    def inner(definition: Definition, node: Dummy):
+        for m in definition.model:
+            if m.type == "object":
+                dummy = Dummy()
+                if m.name:
+                    dummy.__setattr__(m.name, Dummy())
+                    dummy = dummy.__getattribute__(m.name)
+                inner(m, dummy)
+                node.__setattr__(m.name, dummy)
+            else:
+                value = switch(m)
+                value = value.__getattribute__(m.name)
+                node.__setattr__(m.name, value)
+
+    root = Dummy()
+    named_root = None
+    if definition.name:
+        root.__setattr__(definition.name, Dummy())
+        named_root = root.__getattribute__(definition.name)
+
+    if named_root:
+        inner(definition, named_root)
+    else:
+        inner(definition, root)
+    return root
+
+
+def switch(definition: Definition) -> Union[Dummy, bool, str, list, float]:
+    t = definition.type.split(":")[0]
+    return {
+        "boolean": boolean,
+        "number": number,
+        "string": string,
+        "array": array,
+        "object": obj
+    }.get(t)(definition)
 
 
 def create_data(definition: Definition) -> Callable:
-    def switch() -> Union[Dummy, bool, str, list, float]:
-        t = definition.type.split(":")[0]
-        return {
-            "boolean": boolean,
-            "number": number,
-            "string": string,
-            "array": array,
-            "objet": obj
-        }.get(t)(definition)
-
-    return switch
+    def create_data_wrapper():
+        return switch(definition)
+    return create_data_wrapper
